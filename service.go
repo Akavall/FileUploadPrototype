8ea992d41923ce93b8b7be5187734848aa59e00b
Kirill <Akavall@gmail.com>
2018-01-20 20:26:21 -0800

    added go file that sets up the web service

---
+++ service.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("file_upload_prototype.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+func main() {
+
+	http.HandleFunc("/", index)
+
+	ip_address := "localhost:8000"
+	fmt.Printf("Running on: %s\n", ip_address)
+
+	err := http.ListenAndServe(ip_address, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
